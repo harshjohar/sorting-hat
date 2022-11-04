f03e89d7126825cca59c1f08bdfb99884cf99032
harsh johar <hpsj3112@gmail.com>
2022-11-05 00:23:52 +0530

    welcome screen

---
+++ sorting_hat/lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:sorting_hat/constants.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Spacer(flex: 2), //2/6
+                  Text(
+                    "Sorting Hat Ft. PEC",
+                    style: Theme.of(context).textTheme.headline4?.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  const Text("Enter your details below"),
+                  const Spacer(), // 1/6
+                  const TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Phone Number",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  const Spacer(), // 1/6
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding:
+                          const EdgeInsets.all(kDefaultPadding * 0.75), // 15
+                      decoration: const BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Lets Go",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button
+                            ?.copyWith(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  const Spacer(flex: 2), // it will take 2/6 spaces
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
