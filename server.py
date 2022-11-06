ea8456a30aa841b25001fb9bc2ad97382835e350
harsh johar <hpsj3112@gmail.com>
2022-11-06 14:04:45 +0530

    new model

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import pickle
 app = Flask(__name__)
 
 model = pickle.load(open('Rnd30Model.pkl', 'rb'))
+lrModel = pickle.load(open('LogisticModel.pkl', 'rb'))
 
 @app.route('/',methods=['GET'])
 def hello():
@@ -18,7 +19,12 @@ def predict():
     output = prediction[0]
     return jsonify(output)
 
+@app.route('/api/lr',methods=['GET', 'POST'])
+def predictWithLR():
+    data = request.get_json(force=True)
+    prediction = lrModel.predict(np.array(data['exp']))
+    output = prediction[0]
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-
