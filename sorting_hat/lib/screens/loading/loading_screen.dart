b5d250ecd6a7c7cafe31995233a50bd54096ed58
harsh johar <hpsj3112@gmail.com>
2022-11-10 01:14:30 +0530

    it just works

--- sorting_hat/lib/screens/loading/loading_screen.dart
+++ sorting_hat/lib/screens/loading/loading_screen.dart
@@ -10,16 +10,3 @@ class LoadingScreen extends StatelessWidget {
     );
   }
 }
-import 'package:flutter/material.dart';
-
-class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Text("Work under progress on local of harshjohar pls wait")),
-    );
-  }
-}
