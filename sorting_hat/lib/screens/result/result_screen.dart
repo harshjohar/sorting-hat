f342e74df7441b6e09205d09fa6475576c109d76
harsh johar <hpsj3112@gmail.com>
2022-11-10 12:31:55 +0530

    and final commit?

--- sorting_hat/lib/screens/result/result_screen.dart
+++ sorting_hat/lib/screens/result/result_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
@@ -6,9 +5,11 @@ import 'package:sorting_hat/controllers/auth_controller.dart';
 import 'package:sorting_hat/screens/developers/developers_screen.dart';
 import 'package:sorting_hat/screens/loading/loading_screen.dart';
 import 'package:sorting_hat/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ResultScreen extends ConsumerWidget {
   const ResultScreen({Key? key}) : super(key: key);
+
   String getHouseName(String? house) {
     switch (house) {
       case 'G':
@@ -26,6 +27,7 @@ class ResultScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: FutureBuilder(
         future: ref.read(authControllerProvider).getUserData(),
@@ -37,27 +39,68 @@ class ResultScreen extends ConsumerWidget {
           return Stack(
             fit: StackFit.expand,
             children: [
+              Image.asset(
+                'assets/Houses/${user?.house}.jpg',
+                height: size.height,
+                fit: BoxFit.cover,
+              ),
               Positioned(
                 top: 50,
                 right: 0,
-                child: CupertinoButton(
-                  child: const Text("Meet the developers"),
+                child: IconButton(
+                  icon: const Icon(Icons.developer_mode),
                   onPressed: () {
                     Get.to(() => const DevelopersScreen());
                   },
                 ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (user != null)
-                    Text(
-                        "Congrats ${user.name}, you are a proud ${getHouseName(user.house!)}."),
-                  Image.asset('assets/Houses/${user?.house}.png'),
-                  Text("Some of traits of ${getHouseName(user?.house)} TODO"),
-                  Traits(house: user?.house),
-                ],
-              )
+              if (user != null)
+                SafeArea(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/icon/trans_icon.png',
+                        height: 100,
+                        width: 100,
+                      ),
+                      const Spacer(flex: 6),
+                      Text(
+                        user.name,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                            fontSize: 60, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ...(houseData[user.house!]!
+                              .map((trait) => Text(trait))
+                              .toList()),
+                        ],
+                      ),
+                      const Spacer(),
+                      OutlinedButton(
+                        onPressed: () {
+                          launchUrl(Uri.parse("https://www.wizardingworld.com/features/hogwarts-house-meanings"));
+                        },
+                        child: const Text(
+                          "View your house",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              // Column(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     if (user != null)
+              //       Text(
+              //           "Congrats ${user.name}, you are a proud ${getHouseName(user.house!)}."),
+              //   ],
+              // )
             ],
           );
         },
@@ -68,7 +111,9 @@ class ResultScreen extends ConsumerWidget {
 
 class Traits extends StatelessWidget {
   const Traits({this.house, super.key});
+
   final String? house;
+
   @override
   Widget build(BuildContext context) {
     return Container(
